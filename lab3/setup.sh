2c731fc96206afcb7c96e2dac25dec84d0ce2969
Jason Lau <i@dotkrnl.com>
2021-02-08 22:15:21 -0800

    feat(lab3): upgrade intel sdk

--- lab3/setup.sh
+++ lab3/setup.sh
@@ -6,9 +6,9 @@ else
   sudo env DEBIAN_FRONTEND=noninteractive apt install aria2 opencl-headers build-essential cpio -y
   tmp=$(mktemp -d)
   pushd "$tmp"
-  aria2c -x16 -c http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13793/l_opencl_p_18.1.0.013.tgz
-  tar xf l_opencl_p_18.1.0.013.tgz
-  pushd l_opencl_p_18.1.0.013
+  aria2c -x16 -c https://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/17206/intel_sdk_for_opencl_applications_2020.3.494.tar.gz
+  tar xf intel_sdk_for_opencl_applications_2020.3.494.tar.gz
+  pushd intel_sdk_for_opencl_applications_2020.3.494
   sed -i 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/' silent.cfg
   sudo ./install.sh --silent silent.cfg
   popd; popd
