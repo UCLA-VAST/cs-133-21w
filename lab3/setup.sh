0f37eac32d8f3a54adac75ec588f919fe4b86762
Jason Lau <i@dotkrnl.com>
2021-02-09 00:14:42 -0800

    fix(lab3): add dependencies missing in some env

--- lab3/setup.sh
+++ lab3/setup.sh
@@ -3,7 +3,7 @@ if [ "$(uname -s)" == 'Darwin' ]; then
   xcode-select --install
 else
   sudo apt update
-  sudo env DEBIAN_FRONTEND=noninteractive apt install aria2 opencl-headers build-essential cpio -y
+  sudo env DEBIAN_FRONTEND=noninteractive apt install aria2 opencl-headers build-essential cpio ocl-icd-libopencl1 libnuma1 libpciaccess0 -y
   tmp=$(mktemp -d)
   pushd "$tmp"
   aria2c -x16 -c https://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/17206/intel_sdk_for_opencl_applications_2020.3.494.tar.gz
