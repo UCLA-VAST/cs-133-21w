558bd3611c44954d9b67cc861e3bd7e7561c5c69
Jason Lau <i@dotkrnl.com>
2021-02-13 23:46:52 -0800

    feat(lab4): enable presistent mode for gpu

--- lab4/setup.sh
+++ lab4/setup.sh
@@ -8,5 +8,6 @@ else
       https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
   sudo apt update
   sudo env DEBIAN_FRONTEND=noninteractive apt install opencl-headers build-essential ocl-icd-libopencl1 ocl-icd-opencl-dev libnuma1 libpciaccess0 cuda -y --no-install-recommends
+  sudo nvidia-smi -i 0 -pm ENABLED
   sudo nvidia-smi -i 0 -ac 2505,1177
 fi
