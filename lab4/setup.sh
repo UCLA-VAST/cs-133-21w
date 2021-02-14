a4722aab881a582bbd142e46df0c43d3c14d56ea
Jason Lau <i@dotkrnl.com>
2021-02-13 23:43:17 -0800

    fix(lab4): add missing dependency

--- lab4/setup.sh
+++ lab4/setup.sh
@@ -7,6 +7,6 @@ else
   sudo apt-key adv --fetch-keys \
       https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
   sudo apt update
-  sudo env DEBIAN_FRONTEND=noninteractive apt install opencl-headers build-essential ocl-icd-libopencl1 libnuma1 libpciaccess0 cuda -y --no-install-recommends
+  sudo env DEBIAN_FRONTEND=noninteractive apt install opencl-headers build-essential ocl-icd-libopencl1 ocl-icd-opencl-dev libnuma1 libpciaccess0 cuda -y --no-install-recommends
   sudo nvidia-smi -i 0 -ac 2505,1177
 fi
