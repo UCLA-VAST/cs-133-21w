a28e03822e9ca4b1f5953822a476b7d544206a64
Jason Lau <i@dotkrnl.com>
2021-02-25 17:37:47 -0800

    feat: add lab5 not working :-)

---
+++ lab5/cnn.cpp
@@ -0,0 +1,49 @@
+void CnnKernel(
+    const float input[kNum][kInImSize][kInImSize],
+    const float weight[kNum][kNum][kKernel][kKernel], const float bias[kNum],
+    float output[kNum][kOutImSize][kOutImSize]) {
+
+  // Allocate memory on heap to avoid stack overflow.
+  static float C[kNum][kImSize][kImSize];
+
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kImSize; ++h) {
+      for (int w = 0; w < kImSize; ++w)
+        C[i][h][w] = bias[i];
+    }
+  }
+
+  // Convolution
+  for (int i = 0; i < kNum; ++i) {
+    for (int j = 0; j < kNum; ++j) {
+      for (int h = 0; h < kImSize; ++h) {
+        for (int w = 0; w < kImSize; ++w) {
+          for (int p = 0; p < kKernel; ++p) {
+            for (int q = 0; q < kKernel; ++q)
+              C[i][h][w] += weight[i][j][p][q] * input[j][h + p][w + q];
+          }
+        }
+      }
+    }
+  }
+
+  // ReLU
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kImSize; ++h) {
+      for (int w = 0; w < kImSize; ++w) {
+        C[i][h][w] = max(0.f, C[i][h][w]);
+      }
+    }
+  }
+
+  // Max pooling
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kOutImSize; ++h) {
+      for (int w = 0; w < kOutImSize; ++w) {
+        output[i][h][w] = max(
+            max(C[i][h * 2][w * 2    ], C[i][h * 2 + 1][w * 2    ]),
+            max(C[i][h * 2][w * 2 + 1], C[i][h * 2 + 1][w * 2 + 1]));
+      }
+    }
+  }
+}
